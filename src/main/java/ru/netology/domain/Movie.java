25b8cb7c18bad0006a0cdd4869862f85f1154fdc
Mary <mmatvienko111@gmail.com>
2022-12-27 22:30:45 +0300

    Change commit

---
+++ src/main/java/ru/netology/domain/Movie.java
@@ -0,0 +1,18 @@
+package ru.netology.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
+public class Movie {
+    private int id;
+    private int productId;
+    private String productName;
+    private int productPrice;
+    private int count;
+}
+
